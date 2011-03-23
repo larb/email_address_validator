97e39ac720a46406e0ee8f41f4fd34bc4be19ac7
Evan Phoenix <ephoenix@engineyard.com>
2011-03-23 00:42:37 -0700

    Add RFC2822 support by porting the RFC grammar directly.
    Ported from: http://www.ietf.org/rfc/rfc2822.txt

--- Rakefile
+++ Rakefile
@@ -10,6 +10,7 @@ task 'gem:release' => 'test:run'
 
 task "parser" do
   sh "kpeg -s -o lib/rfc-822/parser.rb -f email.kpeg"
+  sh "kpeg -s -o lib/rfc-822/parser2822.rb -f rfc2822.kpeg"
 end
 
 Bones {
--- spec/rfc-822_spec.rb
+++ spec/rfc-822_spec.rb
@@ -19,7 +19,7 @@ describe RFC822 do
              "foor@bar.newTLD", "domainStartsWithDash@-domain.com",
              "local@SecondLevelDomainNamesValidEvenIfTheyAreLongerThan64Charactersss.org",
              "ipsaredomains@127.0.0.1.26", "domainEndsWithDash@domain-.com",
-             "numbersInTLD@domain.c0m", "IPInsteadOfDomain@127.0.0.1", 
+             "numbersInTLD@domain.c0m", "IPInsteadOfDomain@127.0.0.1"
   ]
    
   VALID_ADDRSPEC = ["l3tt3rsAndNumb3rs@domain.com", "has-dash@domain.com"]
@@ -35,7 +35,7 @@ describe RFC822 do
              "shortipv6@[::1]", "fullipv6@[2001:0DB7:1982:A098:2001:0DB7:1982:A098]",
              "CharsInDomain@! \"#\$%(),/;<>_[]`|.org",
              "toomanyoctets@[2001:0DB7:1982:A098:2001:0DB7:1982:A098:8991]",
-             "garbageipv6@[nthueonthueonhurc]",
+             "garbageipv6@[nthueonthueonhurc]"
   ]
   
   VALID.each do |addr|
@@ -53,6 +53,64 @@ describe RFC822 do
   VALID_ADDRSPEC.each do |addr|
     it "should recognize <#{addr}> as valid" do
       RFC822.validate_addr(addr).should be_true
+    end
+  end
+
+  VALID_MODERN =
+            ["simple@example.com", "\"Abc\@def\"@example.com",
+             "\"Fred Bloggs\"@example.com", "\"Joe\\Blow\"@example.com",
+             "\"Abc@def\"@example.com", "customer/department=shipping@example.com",
+             "$A12345@example.com", "!def!xyz%abc@example.com",
+             "c@(Chris's host.)public.example",
+             "\"Chris Jones\" <c@a.test>",
+             "Group:\"Chris Jones\" <c@a.test>,joe@where.test,John <jdoe@one.test>;",
+             "A Group:\"Chris Jones\" <c@a.test>,joe@where.test,John <jdoe@one.test>;",
+             "l3tt3rsAndNumb3rs@domain.com", "has-dash@domain.com",
+             "hasApostrophe.o'leary@domain.org", "uncommonTLD@domain.museum",
+             "lettersInDomain@911.com", "underscore_inLocal@domain.net",
+             "subdomain@sub.domain.com", "local@dash-inDomain.com",
+             "dot.inLocal@foo.com", "a@singleLetterLocal.org",
+             "singleLetterDomain@x.org", "&*=?^+{}'~@validCharsInLocal.net",
+             "foor@bar.newTLD", "domainStartsWithDash@-domain.com",
+             "local@SecondLevelDomainNamesValidEvenIfTheyAreLongerThan64Charactersss.org",
+             "ipsaredomains@127.0.0.1.26", "domainEndsWithDash@domain-.com",
+             "numbersInTLD@domain.c0m", "IPInsteadOfDomain@127.0.0.1",
+
+             "shortipv6@[::1]", "fullipv6@[2001:0DB7:1982:A098:2001:0DB7:1982:A098]",
+             "toomanyoctets@[2001:0DB7:1982:A098:2001:0DB7:1982:A098:8991]",
+             "garbageipv6@[nthueonthueonhurc]",
+             "missingDot@com"
+  ]
+
+
+  INVALID_MODERN =
+            ["NotAnEmail", "@NotAnEmail", "nodomain@", "missingDomain@.com",
+             "@missingLocal.org", "missingatSign.net",
+             "two@@signs.com", "colonButNoPort@127.0.0.1:",
+             ".localStartsWithDot@domain.com",
+             "localEndsWithDot.@domain.com", "two..consecutiveDots@domain.com",
+             "missingTLD@domain.",
+             "! \"#\$%(),/;<>[]`|@CharsInLocal.org",
+             "IPAndPort@127.0.0.1:25",
+             "CharsInDomain@! \"#\$%(),/;<>_[]`|.org",
+  ]
+
+
+  VALID_MODERN.each do |addr|
+    it "should recognize modern <#{addr}> as valid" do
+      RFC822.validate_modern(addr).should be_true
+    end
+  end
+
+  INVALID_MODERN.each do |addr|
+    it "should recognize modern <#{addr}> as invalid" do
+      RFC822.validate_modern(addr).should be_false
+    end
+  end
+
+  VALID_ADDRSPEC.each do |addr|
+    it "should recognize modern <#{addr}> as valid" do
+      RFC822.validate_modern_addr(addr).should be_true
     end
   end
 end
