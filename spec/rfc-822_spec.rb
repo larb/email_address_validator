610c18fc2dfff310f11a5f226e83ebb5a4559add
Evan Phoenix <ephoenix@engineyard.com>
2011-03-23 09:52:06 -0700

    Cleanup and fix the specs for 822 vs. 2822 differences

--- spec/rfc-822_spec.rb
+++ spec/rfc-822_spec.rb
@@ -2,11 +2,11 @@
 require File.join(File.dirname(__FILE__), %w[spec_helper])
 
 describe RFC822 do
-  VALID   = ["simple@example.com", "\"Abc\@def\"@example.com", 
+  VALID_COMMON   =
+            ["simple@example.com", "\"Abc\@def\"@example.com",
              "\"Fred Bloggs\"@example.com", "\"Joe\\Blow\"@example.com",
              "\"Abc@def\"@example.com", "customer/department=shipping@example.com",
              "$A12345@example.com", "!def!xyz%abc@example.com",
-             "c@(Chris's host.)public.example",
              "\"Chris Jones\" <c@a.test>",
              "Group:\"Chris Jones\" <c@a.test>,joe@where.test,John <jdoe@one.test>;", 
              "A Group:\"Chris Jones\" <c@a.test>,joe@where.test,John <jdoe@one.test>;", 
@@ -19,25 +19,32 @@ describe RFC822 do
              "foor@bar.newTLD", "domainStartsWithDash@-domain.com",
              "local@SecondLevelDomainNamesValidEvenIfTheyAreLongerThan64Charactersss.org",
              "ipsaredomains@127.0.0.1.26", "domainEndsWithDash@domain-.com",
-             "numbersInTLD@domain.c0m", "IPInsteadOfDomain@127.0.0.1"
-  ]
-   
-  VALID_ADDRSPEC = ["l3tt3rsAndNumb3rs@domain.com", "has-dash@domain.com"]
+             "numbersInTLD@domain.c0m", "IPInsteadOfDomain@127.0.0.1",
 
-  INVALID = ["NotAnEmail", "@NotAnEmail", "nodomain@", "missingDomain@.com",
-             "@missingLocal.org", "missingatSign.net", "missingDot@com",
-             "two@@signs.com", "colonButNoPort@127.0.0.1:", 
+             "shortipv6@[::1]", "fullipv6@[2001:0DB7:1982:A098:2001:0DB7:1982:A098]",
+             "toomanyoctets@[2001:0DB7:1982:A098:2001:0DB7:1982:A098:8991]",
+             "garbageipv6@[nthueonthueonhurc]",
+             "missingDot@com"
+  ]
+
+  INVALID_COMMON =
+            ["NotAnEmail", "@NotAnEmail", "nodomain@", "missingDomain@.com",
+             "@missingLocal.org", "missingatSign.net",
+             "two@@signs.com", "colonButNoPort@127.0.0.1:",
              ".localStartsWithDot@domain.com",
              "localEndsWithDot.@domain.com", "two..consecutiveDots@domain.com",
              "missingTLD@domain.",
              "! \"#\$%(),/;<>[]`|@CharsInLocal.org",
              "IPAndPort@127.0.0.1:25",
-             "shortipv6@[::1]", "fullipv6@[2001:0DB7:1982:A098:2001:0DB7:1982:A098]",
              "CharsInDomain@! \"#\$%(),/;<>_[]`|.org",
-             "toomanyoctets@[2001:0DB7:1982:A098:2001:0DB7:1982:A098:8991]",
-             "garbageipv6@[nthueonthueonhurc]"
   ]
-  
+
+  VALID   = VALID_COMMON  + [ "c@(Chris's host.)public.example" ]
+
+  VALID_ADDRSPEC = ["l3tt3rsAndNumb3rs@domain.com", "has-dash@domain.com"]
+
+  INVALID = INVALID_COMMON
+
   VALID.each do |addr|
     it "should recognize <#{addr}> as valid" do
       RFC822.validate(addr).should be_true
@@ -56,45 +63,9 @@ describe RFC822 do
     end
   end
 
-  VALID_MODERN =
-            ["simple@example.com", "\"Abc\@def\"@example.com",
-             "\"Fred Bloggs\"@example.com", "\"Joe\\Blow\"@example.com",
-             "\"Abc@def\"@example.com", "customer/department=shipping@example.com",
-             "$A12345@example.com", "!def!xyz%abc@example.com",
-             "c@(Chris's host.)public.example",
-             "\"Chris Jones\" <c@a.test>",
-             "Group:\"Chris Jones\" <c@a.test>,joe@where.test,John <jdoe@one.test>;",
-             "A Group:\"Chris Jones\" <c@a.test>,joe@where.test,John <jdoe@one.test>;",
-             "l3tt3rsAndNumb3rs@domain.com", "has-dash@domain.com",
-             "hasApostrophe.o'leary@domain.org", "uncommonTLD@domain.museum",
-             "lettersInDomain@911.com", "underscore_inLocal@domain.net",
-             "subdomain@sub.domain.com", "local@dash-inDomain.com",
-             "dot.inLocal@foo.com", "a@singleLetterLocal.org",
-             "singleLetterDomain@x.org", "&*=?^+{}'~@validCharsInLocal.net",
-             "foor@bar.newTLD", "domainStartsWithDash@-domain.com",
-             "local@SecondLevelDomainNamesValidEvenIfTheyAreLongerThan64Charactersss.org",
-             "ipsaredomains@127.0.0.1.26", "domainEndsWithDash@domain-.com",
-             "numbersInTLD@domain.c0m", "IPInsteadOfDomain@127.0.0.1",
+  VALID_MODERN = VALID_COMMON
 
-             "shortipv6@[::1]", "fullipv6@[2001:0DB7:1982:A098:2001:0DB7:1982:A098]",
-             "toomanyoctets@[2001:0DB7:1982:A098:2001:0DB7:1982:A098:8991]",
-             "garbageipv6@[nthueonthueonhurc]",
-             "missingDot@com"
-  ]
-
-
-  INVALID_MODERN =
-            ["NotAnEmail", "@NotAnEmail", "nodomain@", "missingDomain@.com",
-             "@missingLocal.org", "missingatSign.net",
-             "two@@signs.com", "colonButNoPort@127.0.0.1:",
-             ".localStartsWithDot@domain.com",
-             "localEndsWithDot.@domain.com", "two..consecutiveDots@domain.com",
-             "missingTLD@domain.",
-             "! \"#\$%(),/;<>[]`|@CharsInLocal.org",
-             "IPAndPort@127.0.0.1:25",
-             "CharsInDomain@! \"#\$%(),/;<>_[]`|.org",
-  ]
-
+  INVALID_MODERN = INVALID_COMMON
 
   VALID_MODERN.each do |addr|
     it "should recognize modern <#{addr}> as valid" do
